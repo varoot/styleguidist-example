ee6f6a8e42388512e3acc28dab0c461081a9771f
Varoot Phasuthadol <varoot@gmail.com>
2020-10-27 11:06:08 -0400

    Demo bug with react-scripts 4.0.0

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,32 +1,4 @@
 module.exports = {
 	title: 'React Style Guide Example',
 	defaultExample: true,
-	webpackConfig: {
-		devServer: {
-			// Docs: https://codesandbox.io/docs/environment#how-can-i-tell-if-i-am-in-a-container-sandbox
-			disableHostCheck: !!process.env.CODESANDBOX_SSE,
-		},
-		module: {
-			rules: [
-				{
-					test: /\.jsx?$/,
-					exclude: /node_modules/,
-					loader: 'babel-loader',
-				},
-				{
-					test: /\.css$/,
-					use: [
-						'style-loader',
-						{
-							loader: 'css-loader',
-							options: {
-								importLoaders: 1,
-								modules: true
-							}
-						},
-					],
-				},
-			],
-		},
-	},
 };
